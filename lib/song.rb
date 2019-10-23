be26844ff5e120b1c4e3551d1d392196f614b412
github username <github email address>
2019-10-23 08:58:52 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,10 @@ class Song
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
+  def self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-
+ 
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
